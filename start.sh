fe724279cec7a91901920d5a756ab63ba993809d
zqs78 <91004501+zqs78@users.noreply.github.com>
2025-10-23 14:36:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-echo "启动Python健康检查服务..."
+echo "启动Xray服务..."
+/usr/local/bin/xray run -config /etc/xray/config.json &
+echo "等待Xray启动..."
+sleep 3
+echo "启动Python健康检查..."
 python3 /app/main.py
